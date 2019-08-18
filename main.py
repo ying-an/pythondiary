46144c07ae242cdc2fb19bf562cf67849eb54f26
ying-an <54224989+ying-an@users.noreply.github.com>
2019-08-18 11:57:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ def home():
     fill.append(f)
   return render_template("index.html",cat=fill)
 
-if__name__=='__main__':
+if __name__=='__main__':
   app.run(debug=True, host="0.0.0.0", port="3000")
